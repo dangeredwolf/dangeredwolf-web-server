ea2dc1fa6de38b99c2b384c10cb76035231bf4d1
dangeredwolf <d@ngeredwolf.me>
2020-02-18 20:16:03 -0500

    0.1?

---
+++ config.js
@@ -0,0 +1,17 @@
+exports.config = {
+    directoryTraversal: true,
+    style: `
+html {
+    background:#000;
+    color:#fff;
+    padding:5px 20px;
+    font-family:apple-system, Segoe UI, Roboto, Verdana, Arial, sans-serif;
+}
+a {
+    text-decoration:none;
+    color:#2196F3
+}
+a:hover {
+    text-decoration:underline
+}`
+}
--- server.js
+++ server.js
@@ -1,23 +1,128 @@
-var http = require("http");
-var https = require("https");
-var http2 = require("http2");
-var url = require("url");
-var fs = require("fs");
+const http = require("http");
+const https = require("https");
+const http2 = require("http2");
+const url = require("url");
+const fs = require("fs");
+const {httpCodes, githubURL} = require("./constants.js");
+const {config} = require("./config.js");
 
-function handleRequest(req, res) {
-  var q = url.parse(req.url, true);
-  console.log(q);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+const options = {};
+const fsoptions = {};
+const defaultHTML = `<!doctype html>
+<html>
+<head>
+<style>
+${config.style}
+</style>
+</head>
+<body>`;
+const defaultHTMLFooter = `<h3><a href="${githubURL}">dangeredwolf http server</a></h3>`
+
+function sanitisePathName(name) {
+    return decodeURI(name.replace(/\/\.\./,"/").replace(/\/\//g,"/"));
+}
+function sanitiseEncodedPathName(name) {
+    return encodeURI(name.replace(/\/\.\./,"/").replace(/\/\//g,"/"));
 }
 
-http.createServer(handleRequest).listen(80);
-http2.createServer(handleRequest).listen(443);
+function sanitiseStringHTML(str) {
+    return str.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/\"/,"&quot;")
+}
+
+function handleErrorPage(code, res) {
+    res.writeHead(code, {'Content-Type': 'text/html'});
+    fs.readFile("./err/"+code+".html", fsoptions, function(err, data) {
+        if (err) {
+            return res.end(
+`${defaultHTML}
+<h1>${code} ${httpCodes[code] || ""}</h1>
+${defaultHTMLFooter}`
+            );
+        }
+        res.writeHead(code, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}
+
+function handleRequest(req, res) {
+    let q;
+    try {
+        q = new URL(req.url, "file:///.");
+    } catch(e) {
+        if (e instanceof TypeError) {
+            return handleErrorPage(400, res);
+        } else {
+            return handleErrorPage(500, res);
+        }
+    }
+    console.log(q);
+    let filename;
+    try {
+        filename = "./dir/" + sanitisePathName(q.pathname);
+    } catch(e) {
+        if (e instanceof URIError) {
+            return handleErrorPage(400, res);
+        } else {
+            return handleErrorPage(500, res);
+        }
+    }
+    try {
+        fs.readdirSync(filename, fsoptions);
+        fs.readdir(filename, fsoptions, function(err, data) {
+            var matched = false;
+            for (i in data) {
+                if (data[i] === "index.js") {
+                    require(filename + "/" + data[i]).default(req, res);
+                } else if (data[i].match("index.") !== null) {
+                    matched = true;
+                    fs.readFile(sanitiseEncodedPathName(filename + "/" + data[i]), fsoptions, function(err, data) {
+                        if (err) {
+                            console.log(err);
+                            return handleErrorPage(404, res);
+                        }
+                        res.setHead("Server","dangeredwolf-node")
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.write(data);
+                        return res.end();
+                    });
+                }
+            }
+            console.log(matched);
+            if (!matched && config.directoryTraversal) {
+
+                let html = defaultHTML;
+                html += `<h1>${sanitiseStringHTML(q.pathname)}</h1>`
+                for (i in data) {
+                    html += `<a href=${sanitiseStringHTML(sanitiseEncodedPathName(q.pathname + "/" + data[i]))}>${sanitiseStringHTML(data[i])}</a><br>`
+                }
+                html += defaultHTMLFooter;
+                res.end(html);
+            } else if (!matched) {
+                return handleErrorPage(404, res);
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        fs.readFile(filename, fsoptions, function(err, data) {
+            if (err) {
+                return handleErrorPage(404, res);
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+
+}
+
+function requestWrapper(req, res) {
+    try {
+        handleRequest(req, res);
+    } catch(e) {
+        return handleErrorPage(500, res);
+    }
+}
+
+http.createServer(options, handleRequest).listen(80);
+http2.createServer(options, handleRequest).listen(443);
